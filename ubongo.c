3b774bdc7266ed3e12d26bc766d080f4528dac61
knedl1k <knedl1k@duck.com>
2023-05-07 11:16:40 +0200

    added init LCD function & better color handling

--- ubongo.c
+++ ubongo.c
@@ -2,7 +2,7 @@
   Project main function template for MicroZed based MZ_APO board
   designed by Petr Porazil at PiKRON
 
-  ubongo.c      - main file
+  ubongo.c     -main file
 
 
   (C) Copyright 2023 by SomiLotr & knedl1k
@@ -41,6 +41,10 @@ uint32_t knobs(void);
 void lcd_frame(void);
 void fbchar(char c,int x, int y, unsigned char scale);
 void fontString(char *word, int x, int y, unsigned char scale);
+void prepare_lcd(void);
+
+#define LCD_HEIGHT 320
+#define LCD_WIDTH 480
 
 union pixel{
   uint16_t d; //data
@@ -51,50 +55,34 @@ union pixel{
   };
 };
 
-union pixel fb[480][320]; //frame buffer
+union pixel fb[LCD_WIDTH][LCD_HEIGHT]; //frame buffer
 
 
 
 
 int main(int argc, char *argv[]){
-
   /* Serialize execution of applications */
-
-  /* Try to acquire lock the first */
-  if (serialize_lock(1) <= 0){ //aby nam nekdo nevzal desku
+  if (serialize_lock(1) <= 0){ /* Try to acquire lock the first */
     printf("System is occupied\n");
-
-    if (1){
+    if (1){ /* Wait till application holding lock releases it or exits */
       printf("Waitting\n");
-      /* Wait till application holding lock releases it or exits */
       serialize_lock(0);
     }
   }
+  
   parlcd_base=map_phys_address(PARLCD_REG_BASE_PHYS,PARLCD_REG_SIZE,0); //0=nechcem to cashovat  
   assert(parlcd_base !=NULL);
   parlcd_hx8357_init(parlcd_base);
 
-  for(unsigned short i=0;i<320;i++){
-    for(unsigned short j=0;j<480;j++){
-      fb[j][i].d=0xffff;
-    }
-  }
 
-
-  fbchar('a',0,0,6);
-  fbchar('b',30,10,6);
-  lcd_frame(); //write to panel all changes from frame_buffer
-
+  
   spiled_base=map_phys_address(SPILED_REG_BASE_PHYS,SPILED_REG_SIZE,0); //0=nechcem to cashovat
   assert(spiled_base!=NULL);
 
   rgb1((union rgb){.g=255});
 
   printf("Hello world\n");
-  fontString("Hello World", 0, -200, 3);
-  fbchar('Z',40,-400,6);
-  
-  lcd_frame();
+  fontString("Hello World", 0, -200, 1);
   //while(1){
     //rgb1((union rgb){.d=knobs()});
   //}
@@ -103,12 +91,19 @@ int main(int argc, char *argv[]){
 
   printf("Goodbye world\n");
 
-  /* Release the lock */
-  serialize_unlock();
+  
+  serialize_unlock(); /* Release the lock */
 
   return 0;
 }
-
+/*initializes the LCD*/
+void prepare_lcd(void){
+  int background_color=0xffff;
+  for(unsigned short i=0;i<LCD_HEIGHT;i++)
+    for(unsigned short j=0;j<LCD_WIDTH;j++)
+      fb[j][i].d=background_color;
+  lcd_frame();
+}
 /*sets RGB1 to specified color*/
 void rgb1(union rgb color){
   uint32_t *ptr=spiled_base+SPILED_REG_LED_RGB1_o;
@@ -117,14 +112,14 @@ void rgb1(union rgb color){
 }
 /*reteurns knobs value*/
 uint32_t knobs(void){
-  uint32_t *knobs=(spiled_base + SPILED_REG_KNOBS_8BIT_o);
+  uint32_t *knobs=(spiled_base+SPILED_REG_KNOBS_8BIT_o);
   return *knobs;
 }
 /*writes all cached changes in frame buffer to LCD*/
 void lcd_frame(void){
   parlcd_write_cmd(parlcd_base, 0x2c);
-  for (unsigned short i = 0; i < 320; i++){
-    for (unsigned short j = 0; j < 480; j++){
+  for (unsigned short i=0; i<LCD_HEIGHT; i++){
+    for (unsigned short j=0; j<LCD_WIDTH; j++){
       parlcd_write_data(parlcd_base, fb[j][i].d);
     }
   }
@@ -134,42 +129,46 @@ void fontString(char *word, int x, int y, unsigned char scale){
   size_t chars=strlen(word); //gets the exact amount of chars to print
   int x_off=0, y_off=0;
   for(size_t i=0;i<chars;++i){
-    printf("vypisuju %c\n",word[i]);
     fbchar(word[i],x+x_off,y+y_off,scale);
     x_off+= font_rom8x16.maxwidth*scale;
     y_off+= 0;
   }
+  lcd_frame(); //write to panel all changes from frame_buffer
 }
 
 
 //This function draws a character 'c' onto a framebuffer 'fb' at position (x, y), with a scaling factor of 'scale'
 void fbchar(char c, int x, int y, unsigned char scale){
-  
+  if(x<0 || x>=LCD_WIDTH || y>0 || y<-LCD_HEIGHT){
+    printf("ERROR: OUT OF LCD RANGE");
+    return;
+  }
+  int char_color=0x0;
   //Check if the character 'c' is within the range of characters defined by the font set 'font_rom8x16'
-  if (c < font_rom8x16.firstchar || c >= (font_rom8x16.size + font_rom8x16.firstchar)){
+  if (c<font_rom8x16.firstchar || c >= (font_rom8x16.size+font_rom8x16.firstchar)){
     //If the character is out of range, use the default character defined by the font set
-    c = font_rom8x16.defaultchar;
+    c=font_rom8x16.defaultchar;
   }
   
   //Calculate the offset of the character 'c' from the first character in the font set, and get a pointer 'cb' to the bit representation of the character
-  const int off = c - font_rom8x16.firstchar;
-  const uint16_t *cb = font_rom8x16.bits + (font_rom8x16.height * off); //char bits
+  const int off=c-font_rom8x16.firstchar;
+  const uint16_t *cb=font_rom8x16.bits+(font_rom8x16.height * off); //char bits
   
   //Iterate over each row and column of the character's bit representation
-  for (unsigned short i = 0; i < font_rom8x16.height; i++){
-    for (unsigned short j = 0; j < font_rom8x16.maxwidth; j++){
+  for (unsigned short i=0; i<font_rom8x16.height; i++){
+    for (unsigned short j=0; j<font_rom8x16.maxwidth; j++){
       
       //Check if the corresponding bit in the character's bit representation is set
-      if (cb[i] & (0x1 << (font_rom8x16.height - 1 - j)) ){
+      if (cb[i] & (0x1 << (font_rom8x16.height-1-j)) ){
         
         //Calculate the position of the corresponding pixel in the framebuffer, taking into account the scaling factor 'scale'
-        int px = x + (j * scale);
-        int py = y + (i * scale);
+        int px=x+(j * scale);
+        int py=y+(i * scale);
         
         //Iterate over a square of size 'scale x scale' and set the color of each pixel to black (represented by the hexadecimal value '0x0')
-        for (unsigned short xi = 0; xi < scale; xi++){
-          for (unsigned short xj = 0; xj < scale; xj++){
-            fb[px + xj][py + xi].d = 0x0;
+        for (unsigned short xi=0; xi<scale; xi++){
+          for (unsigned short xj=0; xj<scale; xj++){
+            fb[px+xj][py+xi].d=char_color;
           }
         }
       }
